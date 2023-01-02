0c7794c5e0d1e9c5a2ffb3e18cc4eda0d9c91f2e
= <=>
2023-01-02 18:52:05 +0330

    create project and add dependency

---
+++ src/main/java/com/brehon/wallet_digipay/WalletDigipayApplication.java
@@ -0,0 +1,13 @@
+package com.brehon.wallet_digipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletDigipayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletDigipayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brehon/wallet_digipay/WalletDigipayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brehon.wallet_digipay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletDigipayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
